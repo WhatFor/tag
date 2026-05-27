2add2f337b1c0a1e38a2ad238d80160faffb7678
WhatFor <chris@chriswalker.soy>
2026-05-27 19:38:40 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "TAG";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+          system:
+          let
+            overlays = [ (import rust-overlay) ];
+            pkgs = import nixpkgs {
+              inherit system overlays;
+            };
+          in
+          {
+            devShells.default =
+              with pkgs;
+              mkShell {
+                buildInputs =
+                  [
+                    # Rust dependencies
+                    (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
+                    pkg-config
+                  ]
+                  ++ lib.optionals (lib.strings.hasInfix "linux" system) [
+                    # for Linux
+                    # Audio (Linux only)
+                    alsa-lib
+                    # Cross Platform 3D Graphics API
+                    vulkan-loader
+                    # For debugging around vulkan
+                    vulkan-tools
+                    # Other dependencies
+                    libudev-zero
+                    xorg.libX11
+                    xorg.libXcursor
+                    xorg.libXi
+                    xorg.libXrandr
+                    libxkbcommon
+                  ];
+                RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+                LD_LIBRARY_PATH = lib.makeLibraryPath [
+                  vulkan-loader
+                  xorg.libX11
+                  xorg.libXi
+                  xorg.libXcursor
+                  libxkbcommon
+                ];
+              };
+          }
+        );
+}
