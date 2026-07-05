f1af5c31c511828b1bb3ff1342f2f7f123f9bca6
WhatFor <chris@chriswalker.soy>
2026-07-05 18:08:08 +0100

    Add enemy assets

---
+++ src/assets/enemy_asset_loader.rs
@@ -0,0 +1,43 @@
+use bevy::asset::{AssetLoader, LoadContext, io::Reader};
+use bevy::prelude::*;
+use serde::Deserialize;
+use thiserror::Error;
+
+#[derive(Asset, TypePath, Deserialize)]
+pub struct EnemyData {
+    pub id: String,
+    pub display_name: String,
+}
+
+#[derive(Default, TypePath)]
+pub struct EnemyAssetLoader;
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+pub enum EnemyAssetLoaderError {
+    #[error("Could not load enemy: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Could not parse enemy RON: {0}")]
+    Ron(#[from] ron::error::SpannedError),
+}
+
+impl AssetLoader for EnemyAssetLoader {
+    type Asset = EnemyData;
+    type Settings = ();
+    type Error = EnemyAssetLoaderError;
+
+    async fn load(
+        &self,
+        reader: &mut dyn Reader,
+        _settings: &(),
+        _load_context: &mut LoadContext<'_>,
+    ) -> Result<Self::Asset, Self::Error> {
+        let mut bytes = Vec::new();
+        reader.read_to_end(&mut bytes).await?;
+        Ok(ron::de::from_bytes(&bytes)?)
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["enem.ron"]
+    }
+}
---
+++ src/assets/enemy_loader.rs
@@ -0,0 +1,61 @@
+use crate::prelude::*;
+use bevy::prelude::*;
+use bevy_asset_loader::prelude::*;
+
+use crate::assets::enemy_asset_loader::EnemyAssetLoader;
+use crate::assets::enemy_asset_loader::EnemyData;
+
+use std::collections::HashMap;
+
+#[derive(AssetCollection, Resource)]
+pub struct EnemyAssets {
+    #[asset(paths("enemies/small_rat.enem.ron"), collection(typed))]
+    handles: Vec<Handle<EnemyData>>,
+}
+
+#[derive(Debug)]
+pub struct EnemyDef {
+    pub id: String,
+    pub display_name: String,
+}
+
+#[derive(Resource, Default, Debug, Deref, DerefMut)]
+pub struct EnemyStore(pub HashMap<String, EnemyDef>);
+
+pub struct EnemyLoaderPlugin;
+
+impl Plugin for EnemyLoaderPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_asset::<EnemyData>();
+        app.init_asset_loader::<EnemyAssetLoader>();
+        app.init_resource::<EnemyStore>();
+
+        app.configure_loading_state(
+            LoadingStateConfig::new(GameState::Initialising).load_collection::<EnemyAssets>(),
+        );
+
+        app.add_systems(OnExit(GameState::Initialising), add_resource);
+    }
+}
+
+fn add_resource(
+    enemies: Res<EnemyAssets>,
+    enemy_data: Res<Assets<EnemyData>>,
+    mut enemy_store: ResMut<EnemyStore>,
+) {
+    for handle in &enemies.handles {
+        let Some(data) = enemy_data.get(handle) else {
+            continue;
+        };
+
+        info!("Parsed Enemy {}...", data.id);
+
+        enemy_store.insert(
+            data.id.clone(),
+            EnemyDef {
+                id: data.id.clone(),
+                display_name: data.display_name.clone(),
+            },
+        );
+    }
+}
